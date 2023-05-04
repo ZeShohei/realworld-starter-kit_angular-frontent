c2d970bbd83bf63dad32df1d4bbac5f71a932047
vgn-df <97737433+vgn-df@users.noreply.github.com>
2023-05-04 14:30:14 +0200

    rwa-1: [NEW] implemented header,footer, changed eslint config and styled with tailwind css

--- src/main.ts
+++ src/main.ts
@@ -7,3 +7,10 @@ import { environment } from './environments/environment';
 if (environment.production) {
 	enableProdMode();
 }
+
+const bootstrapPromise = platformBrowserDynamic().bootstrapModule(AppModule);
+
+// Logging bootstrap information
+bootstrapPromise
+	.then((success) => console.log(`Bootstrap success`))
+	.catch((err) => console.error(err));
